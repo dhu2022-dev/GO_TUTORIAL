1e6042bd9a1dc896e0968fc5e13af7d153124d3f
David Hu <who.is.david101@gmail.com>
2025-06-13 15:43:23 -0400

    Complete Return a random greeting.

--- Tutorials/greetings/greetings.go
+++ Tutorials/greetings/greetings.go
@@ -3,8 +3,10 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
+// Returns a greeting for the named person.
 func Hello(name string) (string, error) {
 	// if no name was given, return error with a message
 	if name == "" {
@@ -15,4 +17,30 @@ func Hello(name string) (string, error) {
 	// in a greeting message
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
+}
+
+func HelloRand(name string) (string, error) {
+	// if no name was given, return error with a message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// create a message using a random format.
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
--- Tutorials/hello/hello.go
+++ Tutorials/hello/hello.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	fmt.Println(quote.Go())
 	fmt.Println(greetings.Hello("Gladys"))
+	fmt.Println(greetings.HelloRand("Gladys"))
 
 	// Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
@@ -19,15 +20,20 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// Request a greeting message.
+	// Request a greeting message. One normal and one random.
 	message, err := greetings.Hello("")
+	messageRand, errRand := greetings.HelloRand("Gladys")
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
 		log.Fatal(err)
 	}
+	if errRand != nil {
+		log.Fatal(errRand)
+	}
 
 	// If no error was returned, print the returned message
 	// to the console.
 	fmt.Println(message)
+	fmt.Println(messageRand)
 }

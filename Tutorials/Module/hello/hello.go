ca7826c937bb1322c3b319ba35f85ba05b8aa095
David Hu <who.is.david101@gmail.com>
2025-06-13 16:14:25 -0400

    Complete greetings for multiple people section

--- Tutorials/Module/hello/hello.go
+++ Tutorials/Module/hello/hello.go
@@ -11,8 +11,7 @@ import (
 
 func main() {
 	fmt.Println(quote.Go())
-	fmt.Println(greetings.Hello("Gladys"))
-	fmt.Println(greetings.HelloRand("Gladys"))
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
 	// Set properties of the predefined Logger, including
 	// the log entry prefix and a flag to disable printing
@@ -21,8 +20,9 @@ func main() {
 	log.SetFlags(0)
 
 	// Request a greeting message. One normal and one random.
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Bob")
 	messageRand, errRand := greetings.HelloRand("Gladys")
+	messages, errMany := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
@@ -31,9 +31,16 @@ func main() {
 	if errRand != nil {
 		log.Fatal(errRand)
 	}
+	if errMany != nil {
+		log.Fatal(errMany)
+	}
 
 	// If no error was returned, print the returned message
 	// to the console.
 	fmt.Println(message)
 	fmt.Println(messageRand)
+	fmt.Println(messages)
+	for name, message := range messages {
+		fmt.Printf("%v: %v\n", name, message)
+	}
 }
